668a3971e466f6761261a40a05603b9bddeebb9d
Akansha_Kumari <Akansha_Kumari@epam.com>
2020-07-15 00:34:09 +0530

    Adding week 4 tasks on Stack and Queue

---
+++ Week-4/MaximumElement.java
@@ -0,0 +1,45 @@
+Hackerrank Link: https://www.hackerrank.com/challenges/maximum-element/problem
+Submission Link: https://www.hackerrank.com/challenges/maximum-element/submissions/code/168884694
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> maxElement = new Stack<>();
+        
+        int query = input.nextInt();
+        while(query-- >0)
+        {
+            int type = input.nextInt();
+            if(type == 1)
+            {
+                int data = input.nextInt();
+                stack.push(data);
+                 if (stack.size() == 1)  
+                 {  
+                   maxElement.push(data);  
+                 }  
+                
+                  if(data > maxElement.peek())
+                       maxElement.push(data);
+                    else
+                       maxElement.push(maxElement.peek());   
+                
+            }     
+            else if(type == 2)
+            {
+                
+                stack.pop();
+                maxElement.pop();
+               
+            }
+            else
+            {
+                System.out.println(maxElement.peek());
+            }
+        }
+        
+    }
+}
